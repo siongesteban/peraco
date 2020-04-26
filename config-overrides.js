a59b57f97a319bde77ed9c4ef1d7e0cc2b062890
siongesteban <siong.esteban@gmail.com>
2020-04-26 15:54:15 +0800

    Add babel-plugin-lodash

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,3 @@
-/* config-overrides.js */
+const { override, addBabelPlugins } = require('customize-cra');
 
-module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  return config;
-};
+module.exports = override(...addBabelPlugins('lodash'));
