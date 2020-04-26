59de5d92147487882058af70e00c76036021b73d
siongesteban <siong.esteban@gmail.com>
2020-04-26 15:32:09 +0800

    Create initial authentication page

---
+++ src/assets/illustrations/index.ts
@@ -0,0 +1,1 @@
+export { ReactComponent as SyncIllustration } from './sync.illustration.svg';
---
+++ src/pages/authentication/authentication.page.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+
+import { SyncIllustration } from 'assets/illustrations';
+import { VerticalCenter } from 'components/common/wrappers';
+import { Button } from 'components/common/inputs';
+
+export const AuthenticationPage: React.FC = () => (
+  <VerticalCenter>
+    <VerticalCenter.Item>
+      <Grid container justify="center">
+        <SyncIllustration />
+      </Grid>
+    </VerticalCenter.Item>
+    <VerticalCenter.Item>
+      <Typography gutterBottom align="center" variant="subtitle2">
+        Use your registered account to enable real-time data synchronization.
+      </Typography>
+    </VerticalCenter.Item>
+    <VerticalCenter.Item>
+      <TextField
+        fullWidth
+        id="outlined-basic"
+        label="Email"
+        placeholder="Enter your email..."
+        type="email"
+        variant="outlined"
+      />
+      <Box my={2}>
+        <Button fullWidth color="primary" size="large">
+          Sign In
+        </Button>
+      </Box>
+      <Box my={2}>
+        <Button fullWidth color="secondary" size="large">
+          Sign Up
+        </Button>
+      </Box>
+    </VerticalCenter.Item>
+  </VerticalCenter>
+);
