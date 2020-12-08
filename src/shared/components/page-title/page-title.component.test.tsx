32e6bf2d57814b64e99bd58ff5e1aa52fb01e006
siongesteban <siong.esteban@gmail.com>
2020-12-09 01:01:00 +0800

    Create unit test for PageTitle

---
+++ src/shared/components/page-title/page-title.component.test.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { render, waitForDomChange } from '@testing-library/react';
+
+import { PageTitle } from './page-title.component';
+
+test('Shows the page title with the right suffix', async () => {
+  const title = 'Home';
+
+  render(<PageTitle title={title} />);
+
+  await waitForDomChange();
+
+  expect(document.querySelector('title')).toHaveTextContent(
+    `${title} – Peraco`,
+  );
+});
