44cd4165bcf29bed688ffd827b8eaf7f8fb98b35
siongesteban <siong.esteban@gmail.com>
2021-01-12 12:55:47 +0800

    Replace react-app-rewired with craco

---
+++ craco.config.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+
+module.exports = {
+  babel: {
+    plugins: [
+      'babel-plugin-transform-typescript-metadata',
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      'lodash',
+      [
+        'import',
+        {
+          libraryName: '@material-ui/core',
+          libraryDirectory: 'esm',
+          camel2DashComponentName: false,
+        },
+        'core',
+      ],
+      [
+        'import',
+        {
+          libraryName: '@material-ui/icons',
+          libraryDirectory: 'esm',
+          camel2DashComponentName: false,
+        },
+        'icons',
+      ],
+    ],
+  },
+};
