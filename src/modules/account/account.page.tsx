74a5226a4237980eac2ccd5a0a75bdc3f5c900c8
siongesteban <siong.esteban@gmail.com>
2021-01-13 22:18:51 +0800

    Set PageTitle in WalletsPage and AccountPage

--- src/modules/account/account.page.tsx
+++ src/modules/account/account.page.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 
 import { Button, Container } from '@material-ui/core';
 
-import { useAuthentication, useSignOut } from 'modules/authentication';
+import { PageTitle } from 'shared/components';
 import { MainPageContainer } from 'modules/app/main';
+import { useAuthentication, useSignOut } from 'modules/authentication';
 
 export const AccountPage: React.FC = () => {
   const signOut = useSignOut();
@@ -16,13 +17,16 @@ export const AccountPage: React.FC = () => {
   };
 
   return (
-    <MainPageContainer>
-      <MainPageContainer.Header title="Account" />
-      <MainPageContainer.Content>
-        <Container>
-          <Button onClick={handleClick}>Sign Out: {user?.name}</Button>
-        </Container>
-      </MainPageContainer.Content>
-    </MainPageContainer>
+    <>
+      <PageTitle title="Account" />
+      <MainPageContainer>
+        <MainPageContainer.Header title="Account" />
+        <MainPageContainer.Content>
+          <Container>
+            <Button onClick={handleClick}>Sign Out: {user?.name}</Button>
+          </Container>
+        </MainPageContainer.Content>
+      </MainPageContainer>
+    </>
   );
 };
--- src/modules/wallets/wallets.page.tsx
+++ src/modules/wallets/wallets.page.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
 
+import { PageTitle } from 'shared/components';
 import { MainPageContainer } from 'modules/app/main';
 
 import { Wallets } from './wallets.component';
 
 export const WalletsPage: React.FC = () => (
-  <MainPageContainer>
-    <MainPageContainer.Header title="Wallets" />
-    <MainPageContainer.Content subdued>
-      <Wallets />
-    </MainPageContainer.Content>
-  </MainPageContainer>
+  <>
+    <PageTitle title="Wallets" />
+    <MainPageContainer>
+      <MainPageContainer.Header title="Wallets" />
+      <MainPageContainer.Content subdued>
+        <Wallets />
+      </MainPageContainer.Content>
+    </MainPageContainer>
+  </>
 );
