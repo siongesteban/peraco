d5345a981a8e7cbe9ed80a45a8dea1e25223827e
siongesteban <siong.esteban@gmail.com>
2021-01-28 10:30:13 +0800

    Apply initial performance and accessibility improvements

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,11 @@ import ReactDOM from 'react-dom';
 
 import { createDb } from 'shared/services/rxdb';
 import { Root } from 'modules/app/root';
-import 'modules/app/root/root.css';
 
 import { registerServiceWorker } from './registerServiceWorker';
 
 const init = async (): Promise<void> => {
   await registerServiceWorker();
-
   await createDb();
 
   ReactDOM.render(<Root />, document.getElementById('root'));
--- src/modules/app/theme/theme-provider.component.tsx
+++ src/modules/app/theme/theme-provider.component.tsx
@@ -1,4 +1,12 @@
 import React from 'react';
+import '@fontsource/nunito/300.css';
+import '@fontsource/nunito/300-italic.css';
+import '@fontsource/nunito/400.css';
+import '@fontsource/nunito/400-italic.css';
+import '@fontsource/nunito/700.css';
+import '@fontsource/nunito/700-italic.css';
+import '@fontsource/nunito/800.css';
+import '@fontsource/nunito/800-italic.css';
 
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 
